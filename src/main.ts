ea5813b6ab77922a6002707ea3b34a49b940c448
whispermind <49441048+whispermind@users.noreply.github.com>
2022-07-23 18:19:11 +0300

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { config } from 'dotenv'
+import { config } from 'dotenv';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { join, dirname } from 'path';
+import { readFile } from 'fs/promises';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const source = await readFile(join(dirname(__dirname), 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(source);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();
