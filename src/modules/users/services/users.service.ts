4bee9006a785811989bbe2109e60c41372039959
whispermind <49441048+whispermind@users.noreply.github.com>
2022-07-29 21:19:23 +0300

    refactor: clean users services

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -5,32 +5,20 @@ import { IUser } from 'src/IData/IData';
 @Injectable()
 export class UsersService {
   async getUsers() {
-    return await store.users;
+
   }
   async getUser(id: string) {
-    return await store.users.find((user) => user.id === id);
+    
   }
   async createUser(user: IUser) {
-    await store.users.push(user);
-    return user;
+   
   }
 
   async updatePassword(update: IUser) {
-    const index = await store.users.findIndex((user) => user.id === update.id);
-    if (index < 0) return false;
-    store.users[index] = update;
-    return update;
+    
   }
 
   async deleteUser(id: string) {
-    let item;
-    store.users = await store.users.filter((user) => {
-      if (user.id === id) {
-        item = user;
-        return false;
-      }
-      return true;
-    });
-    return item || false;
+    
   }
 }
